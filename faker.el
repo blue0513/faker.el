2360d9df5a6ce9ff22c44e69ab5895a99d43e0b5
Taiju Aoki <aokitaiju0513@gmail.com>
2018-10-03 01:18:47 +0900

    update faker() & cycle-fake-genuine()

--- faker.el
+++ faker.el
@@ -20,6 +20,8 @@
 ;; URL: https://github.com/blue0513
 ;; Version: 0.0.1
 
+;;; Commentary:
+
 ;; Load this script and set a directory to save fake files.
 ;;
 ;;   (require 'faker)
@@ -28,12 +30,14 @@
 
 ;;; Code:
 
+(require 'subr-x)
+
 (defgroup faker nil
   "fake to genuine, genuine to fake"
   :group 'emacs)
 
 (defcustom faker-directory "/path/to/save/dir/"
-  "directory used to store fake files"
+  "Directory used to store fake files."
   :group 'faker)
 
 (define-minor-mode faker-minor-mode
@@ -43,50 +47,70 @@
   :lighter " Faker")
 
 (defun faker--fake-file-name (name)
-  "get the fake file path for this buffer"
+  "Get the fake file path for this buffer NAME."
   (concat faker-directory
 	  (replace-regexp-in-string "[/:]" "!" name)))
 
 (defun faker--genuine-file-name (name)
-  "get the genuine file path for this buffer"
+  "Get the genuine file path for this buffer NAME."
   (replace-regexp-in-string
    "[!:]" "/"
    (string-remove-prefix faker-directory name)))
 
-(defun genuine-to-fake()
-  (interactive)
+(defun faker--find-fake()
+  "Find the fake file if exists."
+  (let* ((maybe-fake-file-path (faker--fake-file-name buffer-file-name)))
+    (if (file-exists-p maybe-fake-file-path)
+	(progn
+	  (find-file maybe-fake-file-path)
+	  (faker-minor-mode 1)
+	  (message "Found fake file!")
+	  (sit-for 0.5))
+      (message "Could'n find fake file..."))))
+
+(defun faker--find-genuine()
+  "Find the genuine file if exists."
+  (let* ((maybe-genuine-file-path (faker--genuine-file-name buffer-file-name)))
+    (if (file-exists-p maybe-genuine-file-path)
+	(progn
+	  (find-file maybe-genuine-file-path)
+	  (message "Found genuine file!")
+	  (faker-minor-mode 0)
+	  (sit-for 0.5))
+      (message "Couldn't find genuine file..."))))
+
+(defun faker--genuine-to-fake()
+  "Create the fake file from the genuine file."
   (let* ((old-file-path (buffer-file-name))
 	 (new-file-path (faker--fake-file-name buffer-file-name)))
     (write-file new-file-path t)
-    (rename-buffer "*Fake*")
-    (faker-minor-mode 1)))
+    (faker-minor-mode 1)
+    (message "Create fake file!")
+    (sit-for 0.5)))
 
-(defun find-fake()
-  (interactive)
-  (let* ((maybe-fake-file-path (faker--fake-file-name buffer-file-name)))
-    (when (file-exists-p maybe-fake-file-path)
-      (find-file maybe-fake-file-path)
-      (rename-buffer "*Fake*")
-      (faker-minor-mode 1)
-      (message "Found fake file!")
-      (sit-for 0.5))))
-
-(defun find-genuine()
-  (interactive)
-  (let* ((maybe-genuine-file-path (faker--genuine-file-name buffer-file-name)))
-    (when (file-exists-p maybe-genuine-file-path)
-      (find-file maybe-genuine-file-path)
-      (message "Found genuine file!")
-      (faker-minor-mode 0)
-      (sit-for 0.5))))
-
-(defun fake-to-genuine()
-  (interactive)
+(defun faker--fake-to-genuine()
+  "Create the genuine file from the fake file."
   (let* ((current-file-path (buffer-file-name))
 	 (genuine-file-path (faker--genuine-file-name current-file-path)))
     (write-file genuine-file-path t)
+    (delete-file current-file-path)
     (faker-minor-mode 0)
-    (delete-file current-file-path)))
+    (message "Create genuine file!")
+    (sit-for 0.5)))
+
+(defun faker()
+  "Create the genuine or fake file."
+  (interactive)
+  (if (bound-and-true-p faker-minor-mode)
+      (faker--fake-to-genuine)
+    (faker--genuine-to-fake)))
+
+(defun cycle-fake-genuine()
+  "Find the genuine or fake file."
+  (interactive)
+  (if (bound-and-true-p faker-minor-mode)
+      (faker--find-genuine)
+    (faker--find-fake)))
 
 ;; * provide
 
